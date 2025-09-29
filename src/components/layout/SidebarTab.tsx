019e26f5fe49cee3cd51a121e7a8398095d1b263
Takuya Numao <takuyanumao@MacBookPro.lan>
2025-09-30 05:05:36 +0900

    side bar keep a consistent   height.

--- src/components/layout/SidebarTab.tsx
+++ src/components/layout/SidebarTab.tsx
@@ -1,6 +1,6 @@
 // src/components/layout/SidebarTab.tsx
 import React from "react";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 interface Props {
   title: string;
@@ -15,12 +15,35 @@ export default function SidebarTab({ title }: Props) {
         justifyContent: "flex-start",
         color: "#fff",
         textTransform: "none",
+        minHeight: 40,
+        paddingY: 0.5,
+        paddingX: 1.5,
+        overflow: "hidden",
+        "& .MuiButton-startIcon": {
+          marginRight: 1,
+        },
+        "& .MuiButton-endIcon": {
+          marginLeft: 1,
+        },
         "&:hover": {
           backgroundColor: "var(--color-hover)",
         },
       }}
     >
-      {title}
+      <Box
+        component="span"
+        sx={{
+          display: "block",
+          flexGrow: 1,
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          textAlign: "left",
+          fontSize: "0.95rem",
+        }}
+      >
+        {title}
+      </Box>
     </Button>
   );
 }
